733ef522f72cba06ad7ba001ccca449ba1ae175b
xinggj <sixtynine@live.cn>
2014-12-23 11:15:13 +0800

    test

--- UDPSend/Program.cs
+++ UDPSend/Program.cs
@@ -16,6 +16,7 @@ namespace UDPSend
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UDPSendFrm());
+            //test
         }
     }
 }
