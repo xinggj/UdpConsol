4a29d5e9faa97844a01b83e6f52299aa6045808c
xinggj <sixtynine@live.cn>
2014-12-23 13:15:56 +0800

    retest

--- UDPSend/Program.cs
+++ UDPSend/Program.cs
@@ -16,7 +16,6 @@ namespace UDPSend
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UDPSendFrm());
-            //test
         }
     }
 }
